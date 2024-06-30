8be8969956456afcd7bf614c155009dc3ffd3698
sgup0024 <sgup0024@student.monash.edu>
2024-06-30 23:54:10 +1000

    Something something

--- app/src/main/java/com/example/finityapp/MainActivity.java
+++ app/src/main/java/com/example/finityapp/MainActivity.java
@@ -1,12 +1,20 @@
 package com.example.finityapp;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +26,27 @@ public class MainActivity extends AppCompatActivity {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+
+            FirebaseDatabase database= FirebaseDatabase.getInstance();
+
+            DatabaseReference myref= database.getReference("messages");
+
+            myref.setValue("hello from our course");
+
+            myref.addValueEventListener(new ValueEventListener() {
+                TextView textview=findViewById(R.id.textView);
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    String value =snapshot.getValue(String.class);
+                    textview.setText(value);
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+
             return insets;
         });
     }
