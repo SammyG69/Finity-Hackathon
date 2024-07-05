720d40966401aea34744125df315651c8227ce23
sgup0024 <sgup0024@student.monash.edu>
2024-07-05 15:31:02 +1000

    Created a welcome page

---
+++ app/src/main/java/com/example/finityapp/WelcomePage.java
@@ -0,0 +1,33 @@
+package com.example.finityapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WelcomePage extends AppCompatActivity
+{
+    Button begin;
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        begin=findViewById(R.id.buttonBegin);
+
+        begin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent j=new Intent(
+                        WelcomePage.this, MainActivity.class
+                );
+
+                startActivity(j);
+            }
+        });
+    }
+}
